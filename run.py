9c49b78985c919c3f91b6a089f0af7d3f05b68c5
Zhazhalu <530972907@qq.com>
2020-07-06 22:29:38 +0800

    第一次提交

---
+++ run.py
@@ -0,0 +1,10 @@
+from HTMLTestRunnerNew import HTMLTestRunner
+from common.project_path import *
+import unittest
+
+
+# # 实例化对象
+# s = unittest.TestSuite()
+# loader = unittest.TestLoader()
+# s.addTests(loader.discover(testcase_dir))
+# s.run()
